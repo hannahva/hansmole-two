6278bd8466e5601af8e202f478e460e59f26d8aa
hannahva <hannah.vanadrichem@gmail.com>
2018-03-02 14:09:51 -0800

    Set root to static_pages#home and remove reference to welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/art'
 
-  get 'welcome/index'
-
-  root 'welcome#index'
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
