6d81fbdaf1f90f5a6bc28ac0ad39c162a1967851
hannahva <hannah.vanadrichem@gmail.com>
2018-03-02 14:05:31 -0800

    Replace controller structure to accomodate static pages better

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/art'
+
   get 'welcome/index'
 
   root 'welcome#index'
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get art" do
+    get static_pages_art_url
+    assert_response :success
+  end
+
+end
